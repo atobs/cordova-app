2f6d31ea9541536ca562258c0c40364ec052c993
okay <okay@chert>
2015-06-12 11:08:52 -0700

    put in atob stuffs

--- www/js/index.js
+++ www/js/index.js
@@ -19,6 +19,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
+        window.location.replace("http://atob.xyz");
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -33,6 +34,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
